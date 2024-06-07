72ce04f9992074dbda82f859e6cc55f528ae799d
Ayman Talat <aymantalat2@gmail.com>
2024-06-07 12:21:29 +0300

    solved the first 7 exercises. Lesson: Arrays and Loops

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+  let resultArr = []
+  
+  for (let i = 0; i < array.length; i++) {
+    let remove = false;
+    for (let j = 0; j < items.length; j++) {
+      if (array[i] === items[j]) {
+        remove = true;
+        break;
+      }
+    }
+    if (!remove) resultArr.push(array[i]);
+  }
+  return resultArr;
 };
 
 // Do not edit below this line
