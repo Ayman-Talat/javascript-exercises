2157639d1525b9c91fdf08287ee0f30c03ee6c56
Ayman Talat <aymantalat2@gmail.com>
2024-06-18 21:48:09 +0300

    solve exercises 08 to 12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+  str = str.split(" ").join("");
+  str = str.toLowerCase();
+  if (str.length <= 1) 
+    return true;
+  if (str[0] !== str[str.length - 1])
+    return false;
+  return palindromes(str.slice(1, str.length - 1));
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((max, person) => {
+    const currentDate = new Date().getFullYear();
+    const age = person.yearOfDeath === undefined ? currentDate - person.yearOfBirth : person.yearOfDeath - person.yearOfBirth;
+    return age > max[1] ? [person, age]: max;
+  }, [undefined, 0])[0];
 };
 
 // Do not edit below this line
